23a88fe3e3372c2b06f6d3f06c854ec4fd253e88
Dhayalin <dhayalin56@gmail.com>
2026-02-10 07:02:40 +0530

    Dice Rolling Game using Python Unicodes and Random methods

---
+++ DiceRoller.py
@@ -0,0 +1,72 @@
+import random
+
+# ● ┌ ─ ┐ │ └ ┘
+
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+
+    2: ("┌─────────┐",
+        "│ ●       │",
+        "│         │",
+        "│       ● │",
+        "└─────────┘"),
+
+    3: ("┌─────────┐",
+        "│ ●       │",
+        "│    ●    │",
+        "│       ● │",
+        "└─────────┘"),
+
+    4: ("┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"),
+
+    5: ("┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"),
+
+    6: ("┌─────────┐",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘")                   
+}
+
+
+dice = []
+total = 0
+
+num_of_dice = int(input("Enter the Number of Dice you want to roll: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+for die in range(num_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+
+for die in dice:
+    total += die
+print("")    
+print(f"Total value of dice is {total}")    
+
+
+
+
+
+
+
+
+
+
+
